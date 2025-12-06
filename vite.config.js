aee1f4a2ef3c80b78602dc6c633c3eabc26e52b9
santhoshsharuk <santhoshsharuk16@gmail.com>
2025-12-06 18:53:00 +0530

    feat: Add about page to Vite build configuration and update contact information on the about page.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         laptops: resolve(__dirname, "laptops.html"),
+        about: resolve(__dirname, "about.html"),
         product: resolve(__dirname, "product.html"),
         adminLogin: resolve(__dirname, "admin/login.html"),
         adminDashboard: resolve(__dirname, "admin/dashboard.html"),
